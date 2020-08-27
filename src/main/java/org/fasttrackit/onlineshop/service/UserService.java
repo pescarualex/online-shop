ea91c58044653843e321da4d44ba072f3d7ccd64
pescarualex <pescarualexflorin@gmail.com>
2020-08-27 21:32:26 +0300

    added getUser method and user service

--- src/main/java/org/fasttrackit/onlineshop/service/UserService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/UserService.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.User;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistence.UserRepository;
 import org.fasttrackit.onlineshop.transfer.SaveUserRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -30,6 +33,22 @@ public class UserService {
         user.setLastName(request.getLastName());
 
         return userRepository.save(user);
+    }
+
+    public User getUser(long id) {
+        LOGGER.info("Retrieving user {}", id);
+
+//        Optional<User> userOptional = userRepository.findById(id);
+//
+//        if (userOptional.isPresent()) {
+//            return userOptional.get();
+//        } else {
+//            throw new ResourceNotFoundException("User " + id + " does not exist");
+//        }
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User " + id + " does not exist"));
+
     }
 
 

7709e079fb88623c6409e889ae6e10832ccc192b
pescarualex <pescarualexflorin@gmail.com>
2020-10-08 20:56:23 +0300

    added product integration test

--- src/test/java/org/fasttrackit/onlineshop/product/ProductServiceIntegrationTest.java
+++ src/test/java/org/fasttrackit/onlineshop/product/ProductServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package org.fasttrackit.onlineshop.product;
 
 import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.steps.ProductTestSteps;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +11,14 @@ public class ProductServiceIntegrationTest {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductTestSteps productTestSteps;
 
-//    public void
+    @Test
+    public void createProduct_whenValidRequest_thanReturnCreatedProduct() {
+        productTestSteps.createProduct();
+    }
+
+    // todo: add other scenario
 
 }
